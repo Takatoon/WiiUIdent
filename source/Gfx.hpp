e473dd1a43c893a685a4126a74e9ed3b605afe49
Takatoon <diego@dalirium.com>
2024-10-31 18:12:59 +0100

    Customize colors

--- source/Gfx.hpp
+++ source/Gfx.hpp
@@ -10,15 +10,15 @@ constexpr uint32_t SCREEN_WIDTH            = 1920;
 constexpr uint32_t SCREEN_HEIGHT           = 1080;
 
 constexpr SDL_Color COLOR_BLACK            = { 0x00, 0x00, 0x00, 0xff };
-constexpr SDL_Color COLOR_WHITE            = { 0xff, 0xff, 0xff, 0xff };
-constexpr SDL_Color COLOR_BACKGROUND       = { 0x1b, 0x1c, 0x20, 0xff };
-constexpr SDL_Color COLOR_ALT_BACKGROUND   = { 0x33, 0x34, 0x39, 0xff };
-constexpr SDL_Color COLOR_HIGHLIGHTED      = { 0x00, 0x91, 0xea, 0xff };
-constexpr SDL_Color COLOR_TEXT             = { 0xf8, 0xf8, 0xf8, 0xff };
+constexpr SDL_Color COLOR_WHITE            = { 0xfc, 0xfc, 0xfc, 0xff };
+constexpr SDL_Color COLOR_BACKGROUND       = { 0x2d, 0x2d, 0x2d, 0xff };
+constexpr SDL_Color COLOR_ALT_BACKGROUND   = { 0x32, 0x32, 0x32, 0xff };
+constexpr SDL_Color COLOR_HIGHLIGHTED      = { 0x61, 0xd8, 0xe7, 0xff };
+constexpr SDL_Color COLOR_TEXT             = { 0xd1, 0xd1, 0xd1, 0xff };
 constexpr SDL_Color COLOR_ALT_TEXT         = { 0xb0, 0xb0, 0xb0, 0xff };
-constexpr SDL_Color COLOR_ACCENT           = { 0x32, 0xe6, 0xa6, 0xff };
-constexpr SDL_Color COLOR_ALT_ACCENT       = { 0x22, 0xb3, 0x7d, 0xff };
-constexpr SDL_Color COLOR_BARS             = { 0x2f, 0x3f, 0x38, 0xff };
+constexpr SDL_Color COLOR_ACCENT           = { 0x35, 0xd2, 0xae, 0xff };
+constexpr SDL_Color COLOR_ALT_ACCENT       = { 0x1f, 0x22, 0x28, 0xff };
+constexpr SDL_Color COLOR_BARS             = { 0x0d, 0x5e, 0x36, 0xff };
 constexpr SDL_Color COLOR_ERROR            = { 0xff, 0x33, 0x33, 0xff };
 constexpr SDL_Color COLOR_WIIU             = { 0x00, 0x95, 0xc7, 0xff };
 
