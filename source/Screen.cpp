a9cad505d20191009a133ccef25b388e0456098c
Takatoon <diego@dalirium.com>
2024-10-31 18:12:02 +0100

    DrawVerticalMenu: Change text color & add space between icon and text

--- source/Screen.cpp
+++ source/Screen.cpp
@@ -79,18 +79,18 @@ int Screen::DrawList(int x, int y, int w, ScreenList items)
 
 int Screen::DrawVerticalMenu(int x, int y, const char* text, int icon, bool selected)
 {
-    SDL_Color color = selected ? Gfx::COLOR_TEXT : Gfx::COLOR_ALT_TEXT;
+    SDL_Color color = selected ? Gfx::COLOR_WHITE : Gfx::COLOR_TEXT;
     
     if (selected) {
         Gfx::DrawRectFilled(x - 38 , y - 42, 466, 122, Gfx::COLOR_BARS);
-        Gfx::DrawRectFilled(x - 38 + 9 , y - 42 + 9, 466 - 18, 122 - 18, Gfx::COLOR_ALT_BACKGROUND);
+        Gfx::DrawRectFilled(x - 38 + 9 , y - 42 + 9, 466 - 18, 122 - 18, Gfx::COLOR_ALT_ACCENT);
         Gfx::DrawRectFilled(x - 38 + 20 , y - 20, 6, 77, Gfx::COLOR_BARS);
     } 
 
     const int iconWidth = Gfx::GetIconWidth(40, icon) + 10;
 
     Gfx::DrawIcon(x, y + 3, 40, color, icon, Gfx::ALIGN_TOP);
-    Gfx::Print(x + iconWidth, y - 5, 40, color, text, Gfx::ALIGN_TOP);
+    Gfx::Print(x + 10 + iconWidth, y - 5, 40, color, text, Gfx::ALIGN_TOP);
 
     return y + 122; 
 }
